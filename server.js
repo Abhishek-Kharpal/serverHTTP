acd26a3f76d212fd273562fe97b024c6e5559a42
Abhishek <abhikharpal@gmail.com>
2023-01-24 16:01:17 +0530

    feat: add functions for POST and DELETE

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-var http = require('http');
+let http = require('http');
+
+const { parse } = require('querystring');
 
 let data = [
   {
@@ -17,16 +19,38 @@ http.createServer((request,response)=>{
   if(request.url==='/'){
     response.write(`${response.statusCode}`);
   }
-  else if(request.url==='/tasks'){
+  else if((request.url==='/tasks')&&(request.method==='GET')){
     data.forEach((element)=>{
       response.write(JSON.stringify(element));
     });
   }
-  else if(request.url==='/tasks?isComplete=true'){
+  else if((request.url==='/tasks?isComplete=true')&&(request.method==='GET')){
     data.forEach((element)=>{
       if(element.isComplete){
         response.write(JSON.stringify(element));
       }
+    });
+  }
+  else if((request.url.includes('/tasks'))&&(request.method==='DELETE')){
+    let urlTree = (request.url.split('/'));
+    let id = parseInt(urlTree[urlTree.length-1]);
+    let bufferData= [];
+    data.forEach((element)=>{
+      if(element.id!==id){
+        bufferData.push(element);
+      }
+    });
+    data=bufferData;
+    data.forEach((element)=>{
+      response.write(JSON.stringify(element));
+    });
+  }
+  else if((request.url==='/tasks')&&(request.method==='POST')){
+    request.on('data', chunk => {
+      data.push(JSON.parse(chunk));
+    });
+    request.on('end',()=>{
+      console.log(data);
     });
   }
   response.end();
